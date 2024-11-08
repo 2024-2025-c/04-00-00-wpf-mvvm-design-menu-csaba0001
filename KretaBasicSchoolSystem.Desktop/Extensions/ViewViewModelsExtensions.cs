30c9da66c726c257fc519016b566344c0ab677c2
csaba <c9burcsa@vasvari.org>
2024-11-08 09:07:53 +0100

    added schoolclassview to viewmodelsextensions

--- KretaBasicSchoolSystem.Desktop/Extensions/ViewViewModelsExtensions.cs
+++ KretaBasicSchoolSystem.Desktop/Extensions/ViewViewModelsExtensions.cs
@@ -2,10 +2,12 @@
 using KretaBasicSchoolSystem.Desktop.ViewModels.ControlPanel;
 using KretaBasicSchoolSystem.Desktop.ViewModels.Login;
 using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolCitizens;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolClasses;
 using KretaBasicSchoolSystem.Desktop.Views;
 using KretaBasicSchoolSystem.Desktop.Views.ControlPanel;
 using KretaBasicSchoolSystem.Desktop.Views.Login;
 using KretaBasicSchoolSystem.Desktop.Views.SchoolCitizens;
+using KretaBasicSchoolSystem.Desktop.Views.SchoolClasses;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace KretaDesktop.Extensions
@@ -47,6 +49,12 @@ namespace KretaDesktop.Extensions
             services.AddSingleton<StudentView>(s => new StudentView()
             {
                 DataContext = s.GetRequiredService<StudentViewModel>()
+            });
+            //SchoolClass
+            services.AddSingleton<SchoolClassesViewModel>();
+            services.AddSingleton<SchoolClassesView>(s => new SchoolClassesView()
+            {
+                DataContext = s.GetRequiredService<SchoolClassesViewModel>()
             });
 
         }
